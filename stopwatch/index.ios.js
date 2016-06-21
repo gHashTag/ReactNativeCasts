9b5d66e2d87328f2274ffb56f5d594bf13ffb568
#gHashTag <raoffonom@icloud.com>
2016-06-21 22:28:20 +0400

    Update index.ios.js

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -1,121 +1,132 @@
-var formatTime = require('minutes-seconds-milliseconds');
-var React = require('react-native');
-var {
-  Text,
-  View,
-  TouchableHighlight,
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
   AppRegistry,
-  StyleSheet
-} = React;
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
 
-var StopWatch = React.createClass({
-  getInitialState: function() {
-    return {
-      timeElapsed: null,
-      running: false,
-      startTime: null,
-      laps: []
-    }
-  },
-  render: function() {
-    return <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.timerWrapper}>
-          <Text style={styles.timer}>
-            {formatTime(this.state.timeElapsed)}
-          </Text>
-        </View>
-        <View style={styles.buttonWrapper}>
-          {this.startStopButton()}
-          {this.lapButton()}
-        </View>
-      </View>
+var formatTime = require('minutes-seconds-milliseconds');
 
-      <View style={styles.footer}>
-        {this.laps()}
-      </View>
-    </View>
-  },
-  laps: function(){
-    return this.state.laps.map(function(time, index){
-      return <View style={styles.lap}>
-        <Text style={styles.lapText}>
-          Lap #{index + 1}
-        </Text>
-        <Text style={styles.lapText}>
-          {formatTime(time)}
-        </Text>
-      </View>
-    });
-  },
-  startStopButton: function() {
+class stopwatch extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    timeElapsed: 0,
+    running: false,
+    startTime: null
+  };
+  }
+
+  startStopButton() {
     var style = this.state.running ? styles.stopButton : styles.startButton;
 
-    return <TouchableHighlight
-      underlayColor="gray"
-      onPress={this.handleStartPress}
-      style={[styles.button, style]}
-      >
-      <Text>
-        {this.state.running ? 'Stop' : 'Start'}
-      </Text>
-    </TouchableHighlight>
-  },
-  lapButton: function() {
-    return <TouchableHighlight
-      style={styles.button}
-      underlayColor="gray"
-      onPress={this.handleLapPress}
-      >
-      <Text>
-        Lap
-      </Text>
-    </TouchableHighlight>
-  },
-  handleLapPress: function() {
-    var lap = this.state.timeElapsed;
+    return (
+      <TouchableHighlight
+        underlayColor="gray"
+        onPress={this.handleStartPress.bind(this)}
+        style={[styles.button, style]}>
+        <Text>
+          {this.state.running ? 'Stop' : 'Start'}
+        </Text>
+      </TouchableHighlight>
+    )
+  }
 
-    this.setState({
-      startTime: new Date(),
-      laps: this.state.laps.concat([lap])
-    });
-  },
-  handleStartPress: function() {
-    if(this.state.running){
+  lapButton() {
+    return (
+      <View style={styles.button}>
+        <Text>
+          Lap
+        </Text>
+      </View>
+    )
+  }
+
+  handleStartPress() {
+    if(!this.state.running) {
       clearInterval(this.interval);
       this.setState({running: false});
+      console.log (this.state);
       return
     }
-
     this.setState({startTime: new Date()});
 
+
     this.interval = setInterval(() => {
+      const { timeElapsed } = this.state; // grab current timeElapsed from the state.
       this.setState({
-        timeElapsed: new Date() - this.state.startTime,
+        timeElapsed: timeElapsed + (new Date() - this.state.startTime),
         running: true
       });
     }, 30);
-  }
-});
 
-var styles = StyleSheet.create({
+  }
+  handleResetPress() {
+    clearInterval(this.interval);
+    this.setState({
+      running: false,
+      timeElapsed: 0
+    });
+  }
+
+  border(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.timerWrapper}>
+            <Text style={styles.timer}>
+              {formatTime(this.state.timeElapsed)}
+            </Text>
+          </View>
+          <View style={styles.buttonWrapper}>
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+        <View style={styles.footer}>
+            <Text>
+                I am List of Laps
+            </Text>
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
   container: {
-    flex: 1, // Fill the entire the screen
+    flex: 1,
     alignItems: 'stretch'
   },
-  header: { // Yellow
+  header: {
     flex: 1
   },
-  footer: { // Blue
+  footer: {
     flex: 1
   },
-  timerWrapper: { // Red
-    flex: 5, // takes up 5/8ths of the available space
+  timerWrapper: {
+    flex: 5,
     justifyContent: 'center',
     alignItems: 'center'
   },
-  buttonWrapper: { // Green
-    flex: 3, // takes up 3/8ths of the available space
+  buttonWrapper: {
+    flex: 3,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center'
@@ -136,14 +147,7 @@ var styles = StyleSheet.create({
   },
   stopButton: {
     borderColor: '#CC0000'
-  },
-  lap: {
-    justifyContent: 'space-around',
-    flexDirection: 'row'
-  },
-  lapText: {
-    fontSize: 30
   }
 });
 
-AppRegistry.registerComponent('stopwatch', () => StopWatch);
+AppRegistry.registerComponent('stopwatch', () => stopwatch);
